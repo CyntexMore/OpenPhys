fa3fb1d9a846dc2cb4961605f7214ab827b01251
CyntexMore <comebackyt20@gmail.com>
2024-09-25 17:34:06 +0200

    feat: added density property to objects

--- src/utils/object.rs
+++ src/utils/object.rs
@@ -6,6 +6,8 @@ pub struct Object {
     pub temperature: f64,
     /// The object's specific heat capacity in [joule per kilogram kelvin](https://metricsystem.net/derived-units/units-whose-names-include-special-names/joule-per-kilogram-kelvin/) (J/kg K).
     pub specific_heat_capacity: f64,
+    /// The object's density in kig per m^3 (kg/m^3).
+    pub density: f64,
 }
 
 impl Object {
@@ -13,15 +15,16 @@ impl Object {
     /// `mass`, a `temperature` and a `specific_heat_capacity` value.
     /// # Examples
     /// ```
-    /// let obj1 = Object::new(1.0, 273.15, 4980.0);
+    /// let obj1 = Object::new(1.0, 273.15, 4980.0, 1.0);
     /// ````
     /// The variable must be mutable if you want to use it in a function that changes one or more
     /// of it's values.
-    pub fn new(mass: f64, temperature: f64, specific_heat_capacity: f64) -> Self {
+    pub fn new(mass: f64, temperature: f64, specific_heat_capacity: f64, density: f64) -> Self {
         Self {
             mass,
             temperature,
             specific_heat_capacity,
+            density,
         }
     }
 }
