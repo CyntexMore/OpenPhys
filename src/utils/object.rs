dca05db61ae410f5b3b765740cf0aa6c54340385
CyntexMore <comebackyt20@gmail.com>
2024-10-02 21:27:35 +0200

    feat: added velocity, kinetic_energy and positon properties to objects, also, changed how new objects can be created

--- src/utils/object.rs
+++ src/utils/object.rs
@@ -8,25 +8,66 @@ pub struct Object {
     pub specific_heat_capacity: f64,
     /// The object's density in kilogram per m^3 (kg/m^3).
     pub density: f64,
+    /// The object's velocity in meter per second (m/s).
+    pub velocity: f64,
+    /// The object's kinetic energy in joules (J).
+    pub kinetic_energy: f64,
+    /// The object's position in a three dimensional space (x, y, z).
+    pub position: [f64; 3],
 }
 
 impl Object {
-    /// You can create a new object using the `Object::new()` function. An object can have a
-    /// `mass`, a `temperature` and a `specific_heat_capacity` value.
-    /// # Examples
+    /// You can create a new object using the `Object::new()` function.
+    ///
+    /// # Usage
     /// ```
     /// use openphys::utils::object::Object;
     ///
-    /// let obj1 = Object::new(1.0, 273.15, 4980.0, 1.0);
-    /// ````
+    /// let obj1 = Object::new(1.0, 293.15, 4186.0, 1000.0, 0.0, 0.0, [0.0, 0.0, 0.0]);
+    /// ```
+    ///
     /// The variable must be mutable if you want to use it in a function that changes one or more
     /// of it's values.
-    pub fn new(mass: f64, temperature: f64, specific_heat_capacity: f64, density: f64) -> Self {
+    pub fn new() -> Self {
         Self {
-            mass,
-            temperature,
-            specific_heat_capacity,
-            density,
+            mass: 0.0,
+            temperature: 0.0,
+            specific_heat_capacity: 0.0,
+            density: 0.0,
+            velocity: 0.0,
+            kinetic_energy: 0.0,
+            position: [0.0, 0.0, 0.0],
+        }
+    }
+
+    /// You can create an object with some reasonable defaults with the `Object::default()`
+    /// function.
+    ///
+    /// # Usage
+    /// ```
+    /// use openphys::utils::object::Object;
+    ///
+    /// let obj1 = Object::default();
+    /// ```
+    /// Also, you can create an object with some changed values and some default values, like this:
+    /// ```
+    /// use openphys::utils::object::Object;
+    ///
+    /// let obj1 = Object {
+    ///     mass: 1.0,
+    ///     temperature: 293.15,
+    ///     ..Object::default()
+    /// };
+    /// ```
+    pub fn default() -> Self {
+        Self {
+            mass: 1.0,
+            temperature: 293.15,
+            specific_heat_capacity: 4186.0,
+            density: 1000.0,
+            velocity: 0.0,
+            kinetic_energy: 0.0,
+            position: [0.0, 0.0, 0.0],
         }
     }
 }
