2c21cc6aea6e18505ed51828a13dea9e107c2818
CyntexMore <comebackyt20@gmail.com>
2024-10-08 00:43:48 +0200

    tests: added some more unit tests

--- tests/gravitational_force_calculation_test.rs
+++ tests/gravitational_force_calculation_test.rs
@@ -16,3 +16,35 @@ fn test_gravitational_force_calculation() {
 
     calculate_gravitational_force(&obj1, &obj2, distance);
 }
+
+#[test]
+fn test2_gravitational_force_calculation() {
+    let distance = 9210.4;
+
+    let obj1 = Object {
+        mass: 595.0,
+        ..Object::default()
+    };
+    let obj2 = Object {
+        mass: 160.5,
+        ..Object::default()
+    };
+
+    calculate_gravitational_force(&obj1, &obj2, distance);
+}
+
+#[test]
+fn test3_gravitational_force_calculation() {
+    let distance = 1590.4;
+
+    let obj1 = Object {
+        mass: 1.0,
+        ..Object::default()
+    };
+    let obj2 = Object {
+        mass: 1.0,
+        ..Object::default()
+    };
+
+    calculate_gravitational_force(&obj1, &obj2, distance);
+}
--- tests/heat_transfer_simulation_test.rs
+++ tests/heat_transfer_simulation_test.rs
@@ -32,3 +32,67 @@ fn test_heat_transfer_simulation() {
     println!("Final temp of obj1: {:.2}K", result.final_temp_obj1);
     println!("Final temp of obj2: {:.2}K", result.final_temp_obj2);
 }
+
+#[test]
+fn test2_heat_transfer_simulation() {
+    let mut obj1 = Object {
+        mass: 50.0,
+        temperature: 480.15,
+        specific_heat_capacity: 12049.0,
+        ..Object::default()
+    };
+    let mut obj2 = Object {
+        mass: 82.0,
+        temperature: 582.62,
+        specific_heat_capacity: 12049.0,
+        ..Object::default()
+    };
+    let time_step = 0.1; // 0.1 seconds
+    let equilibrium_threshold = 1e-6; // The equilibrium threshold is 10^-6 because
+                                      // trying to check if two f64 values are equal
+                                      // would cause an infinite (or close to
+                                      // infinite) loop
+
+    let result = simulate_heat_transfer(&mut obj1, &mut obj2, time_step, equilibrium_threshold);
+
+    println!("Time steps: {}", result.time_steps);
+    println!("Total time elapsed: {:.2}s", result.total_time);
+    println!(
+        "Total heat transferred: {:.2}K",
+        result.total_heat_transferred
+    );
+    println!("Final temp of obj1: {:.2}K", result.final_temp_obj1);
+    println!("Final temp of obj2: {:.2}K", result.final_temp_obj2);
+}
+
+#[test]
+fn test3_heat_transfer_simulation_high_accuracy() {
+    let mut obj1 = Object {
+        mass: 50.0,
+        temperature: 480.15,
+        specific_heat_capacity: 12049.0,
+        ..Object::default()
+    };
+    let mut obj2 = Object {
+        mass: 82.0,
+        temperature: 582.62,
+        specific_heat_capacity: 12049.0,
+        ..Object::default()
+    };
+    let time_step = 0.1; // 0.1 seconds
+    let equilibrium_threshold = 1e-12; // The equilibrium threshold is 10^-12 because
+                                       // trying to check if two f64 values are equal
+                                       // would cause an infinite (or close to
+                                       // infinite) loop
+
+    let result = simulate_heat_transfer(&mut obj1, &mut obj2, time_step, equilibrium_threshold);
+
+    println!("Time steps: {}", result.time_steps);
+    println!("Total time elapsed: {:.2}s", result.total_time);
+    println!(
+        "Total heat transferred: {:.2}K",
+        result.total_heat_transferred
+    );
+    println!("Final temp of obj1: {:.2}K", result.final_temp_obj1);
+    println!("Final temp of obj2: {:.2}K", result.final_temp_obj2);
+}
