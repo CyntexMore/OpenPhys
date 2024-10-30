41faeac6d0a80c9860f7784bf7764c3a474802b9
CyntexMore <comebackyt20@gmail.com>
2024-10-30 05:00:12 +0100

    test: update sandbox_test.rs to make it work with the latest changes of some other elements

--- tests/sandbox_test.rs
+++ tests/sandbox_test.rs
@@ -1,16 +1,45 @@
-use openphys::modules::sandbox::start_sandbox;
-use openphys::utils::{object::Object, environment::Environment};
+use openphys::modules::sandbox::init_sandbox;
+use openphys::utils::{environment::Environment, object::Object};
 
 #[test]
 fn test_sandbox_start() {
     let mut objects = vec![
-        Object { mass: 1.0, position: [10.4, 2.4, 3.8], ..Object::default() },
-        Object { mass: 1.0, position: [3.4, 1.0, 10.4], ..Object::default() },
+        Object {
+            mass: 1.0,
+            position: [10.4, 2.4, 3.8],
+            ..Object::default()
+        },
+        Object {
+            mass: 1.0,
+            position: [3.4, 1.0, 10.4],
+            ..Object::default()
+        },
     ];
 
     let mut env = Environment::default();
 
     let time_step: f64 = 0.1;
 
-    start_sandbox(&mut env, &mut objects, time_step);
+    init_sandbox(&mut env, &mut objects, time_step);
+}
+
+#[test]
+fn test_sandbox_obj_move() {
+    let mut obj1 = Object {
+        position: [0.0, 0.0, 0.0],
+        ..Object::default()
+    };
+    println!("obj1's position: {:?}", obj1.position);
+
+    obj1.move_to(1.0, 1.0, 1.0);
+    println!("obj1's position: {:?}", obj1.position);
+
+    obj1.move_x(-1.0);
+    println!("obj1's position: {:?}", obj1.position);
+
+    obj1.move_y(-1.0);
+    println!("obj1's position: {:?}", obj1.position);
+
+    obj1.move_z(-1.0);
+    println!("obj1's position: {:?}", obj1.position);
 }
